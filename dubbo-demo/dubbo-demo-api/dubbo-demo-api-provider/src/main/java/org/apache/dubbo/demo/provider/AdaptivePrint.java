a1d1140929afb05f9d6ba81266db1d80f46ab9c8
HeCG <jacktanger@163.com>
2024-03-04 11:53:22 +0800

    [M] add example.

--- dubbo-demo/dubbo-demo-api/dubbo-demo-api-provider/src/main/java/org/apache/dubbo/demo/provider/AdaptivePrint.java
+++ dubbo-demo/dubbo-demo-api/dubbo-demo-api-provider/src/main/java/org/apache/dubbo/demo/provider/AdaptivePrint.java
@@ -2,13 +2,15 @@ package org.apache.dubbo.demo.provider;
 
 import org.apache.dubbo.common.extension.AdaptiveClassCodeGenerator;
 import org.apache.dubbo.registry.RegistryFactory;
+import org.apache.dubbo.rpc.Protocol;
 
 public class AdaptivePrint {
 
     public static void main(String[] args) {
 
         AdaptiveClassCodeGenerator generator =
-                new AdaptiveClassCodeGenerator(RegistryFactory.class, "dubbo");
+//                new AdaptiveClassCodeGenerator(RegistryFactory.class, "dubbo");
+                new AdaptiveClassCodeGenerator(Protocol.class, "dubbo");
         System.out.println(generator.generate());
 
     }
