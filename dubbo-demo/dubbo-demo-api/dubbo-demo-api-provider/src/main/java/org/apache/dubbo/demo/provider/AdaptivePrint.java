e272bf2299ee96f38bcfcbe98e89ca37015e839b
HeCG <jacktanger@163.com>
2024-03-08 17:20:26 +0800

    [M] add example.

--- dubbo-demo/dubbo-demo-api/dubbo-demo-api-provider/src/main/java/org/apache/dubbo/demo/provider/AdaptivePrint.java
+++ dubbo-demo/dubbo-demo-api/dubbo-demo-api-provider/src/main/java/org/apache/dubbo/demo/provider/AdaptivePrint.java
@@ -8,6 +8,7 @@ import org.apache.dubbo.remoting.Dispatcher;
 import org.apache.dubbo.remoting.exchange.Exchanger;
 import org.apache.dubbo.remoting.zookeeper.ZookeeperTransporter;
 import org.apache.dubbo.rpc.Protocol;
+import org.apache.dubbo.rpc.ProxyFactory;
 
 public class AdaptivePrint {
 
@@ -20,7 +21,8 @@ public class AdaptivePrint {
 //                new AdaptiveClassCodeGenerator(Serialization.class, "hessian2");
 //                new AdaptiveClassCodeGenerator(ThreadPool.class, "fixed");
 //                new AdaptiveClassCodeGenerator(Dispatcher.class, "all");
-                new AdaptiveClassCodeGenerator(Exchanger.class, "header");
+//                new AdaptiveClassCodeGenerator(Exchanger.class, "header");
+                new AdaptiveClassCodeGenerator(ProxyFactory.class, "javassist");
         System.out.println(generator.generate());
 
     }
